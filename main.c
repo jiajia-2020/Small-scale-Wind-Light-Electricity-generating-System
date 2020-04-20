88ac3af24ea3ab2946b5c25dbf16d7fee70c794a
jiajia-2020 <59986573+jiajia-2020@users.noreply.github.com>
2020-04-20 18:13:05 +0800

    Create main.c

---
+++ main.c
@@ -0,0 +1,355 @@
+#include "sys.h"
+#include "delay.h"
+#include "usart.h"
+#include "sram.h"
+#include "malloc.h"
+#include "ILI93xx.h"
+#include "led.h"
+#include "beep.h"
+#include "timer.h"
+#include "touch.h"
+#include "sdio_sdcard.h"
+#include "GUI.h"
+#include "ff.h"
+#include "exfuns.h"
+#include "w25qxx.h"
+#include "includes.h"
+#include "ButtonUse.h"
+#include "WM.h"
+#include "DIALOG.h"
+#include "adc.h"
+#include "adc3.h"
+#include "lsens.h"
+#include "usart3.h"
+#include "timer.h"
+#include "gizwits_protocol.h"
+#include "key.h"
+
+
+//WIFI连接状态
+//wifi_sta 0: 断开
+//         1: 已连接
+u8 wifi_sta=0;
+extern dataPoint_t currentDataPoint;
+
+
+
+//任务优先级
+#define START_TASK_PRIO				3
+//任务堆栈大小	
+#define START_STK_SIZE 				1024
+//任务控制块
+OS_TCB StartTaskTCB;
+//任务堆栈	
+CPU_STK START_TASK_STK[START_STK_SIZE];
+//任务函数
+void start_task(void *p_arg);
+
+//TOUCH任务
+//设置任务优先级
+#define TOUCH_TASK_PRIO				4
+//任务堆栈大小
+#define TOUCH_STK_SIZE				128
+//任务控制块
+OS_TCB TouchTaskTCB;
+//任务堆栈
+CPU_STK TOUCH_TASK_STK[TOUCH_STK_SIZE];
+//touch任务
+void touch_task(void *p_arg);
+
+//LED0任务
+//设置任务优先级
+#define LED0_TASK_PRIO 				5
+//任务堆栈大小
+#define LED0_STK_SIZE				128
+//任务控制块
+OS_TCB Led0TaskTCB;
+//任务堆栈
+CPU_STK LED0_TASK_STK[LED0_STK_SIZE];
+//led0任务
+void led0_task(void *p_arg);
+
+//EMWINDEMO任务
+//设置任务优先级
+#define EMWINDEMO_TASK_PRIO			6
+//任务堆栈大小
+#define EMWINDEMO_STK_SIZE			2048
+//任务控制块
+OS_TCB EmwindemoTaskTCB;
+//任务堆栈
+CPU_STK EMWINDEMO_TASK_STK[EMWINDEMO_STK_SIZE];
+//emwindemo_task任务
+void emwindemo_task(void *p_arg);
+/*
+#define WATCH_TASK_PRIO			7
+//任务堆栈大小
+#define WATCH_STK_SIZE			128
+//任务控制块
+OS_TCB WatchTaskTCB;
+//任务堆栈
+CPU_STK WATCH_TASK_STK[WATCH_STK_SIZE];
+//emwindemo_task任务
+void watch_task(void *p_arg);
+*/
+
+
+//按键任务
+//设置任务优先级
+#define KEY_TASK_PRIO       			7
+//设置任务堆栈大小
+#define KEY_STK_SIZE  		    		128
+//任务堆栈	
+OS_TCB KeyTaskTCB;
+//任务堆栈
+CPU_STK KEY_TASK_STK[KEY_STK_SIZE];
+//任务函数
+void key_task(void *p_arg);
+
+
+
+void Gizwits_Init(void)
+{
+	TIM3_Int_Init(10-1,8400-1);	//1MS系统定时 
+	usart3_init(9600);//WIFI初始化 
+	memset((uint8_t*)&currentDataPoint, 0, sizeof(dataPoint_t));//设备状态结构体初始化
+	gizwitsInit();//缓冲区初始化
+}
+
+
+
+//按键处理任务
+//扫描按键，完成开关、自动调整、WIFI 复位/AirLink等控制
+void key_task(void *pdata)
+{
+	u8 key;
+	while(1)
+	{
+		key=KEY_Scan(0);
+		switch(key)
+		{
+			case KEY2_PRES: 	//进入WIFI AirLink模式
+				printf("WIFI进入AirLink连接模式\r\n");
+				gizwitsSetMode(WIFI_AIRLINK_MODE);	//Air-link模式接入
+				break;
+			case WKUP_PRES: 	//复位WIFI
+				printf("WIFI复位，请重新配置连接\r\n");
+				gizwitsSetMode(WIFI_RESET_MODE);	//复位WIFI
+				break;
+		}
+		delay_ms(10);
+	}
+}
+
+
+
+int main(void)
+{
+	OS_ERR err;
+	CPU_SR_ALLOC();
+	
+	delay_init(168);       	//延时初始化
+	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2); 	//中断分组配置
+	uart_init(115200);    	//串口波特率设置
+	TFTLCD_Init();			//初始化LCD
+	W25QXX_Init();			//初始化W25Q128
+	LED_Init();   			//LED初始化
+		Adc_Init();         //内部温度传感器ADC初始化
+			Gizwits_Init();			//初始化机智云及相关外设 
+		KEY_Init();				//初始化按键 
+			Lsens_Init(); 			//初始化光敏传感器
+	
+	BEEP_Init();			//初始化蜂鸣器
+	FSMC_SRAM_Init(); 		//SRAM初始化	
+	mem_init(SRAMIN); 		//内部RAM初始化
+	mem_init(SRAMEX); 		//外部RAM初始化
+	mem_init(SRAMCCM);		//CCM初始化
+	
+	exfuns_init();			//为fatfs文件系统分配内存
+	f_mount(fs[0],"0:",1);	//挂载SD卡
+	f_mount(fs[1],"1:",1);	//挂载FLASH
+
+	TP_Init();				//初始化触摸屏
+	OSInit(&err);			//初始化UCOSIII
+	OS_CRITICAL_ENTER();	//进入临界区
+	//创建开始任务
+	OSTaskCreate((OS_TCB 	* )&StartTaskTCB,		//任务控制块
+				 (CPU_CHAR	* )"start task", 		//任务名字
+                 (OS_TASK_PTR )start_task, 			//任务函数
+                 (void		* )0,					//传递给任务函数的参数
+                 (OS_PRIO	  )START_TASK_PRIO,     //任务优先级
+                 (CPU_STK   * )&START_TASK_STK[0],	//任务堆栈基地址
+                 (CPU_STK_SIZE)START_STK_SIZE/10,	//任务堆栈深度限位
+                 (CPU_STK_SIZE)START_STK_SIZE,		//任务堆栈大小
+                 (OS_MSG_QTY  )0,					//任务内部消息队列能够接收的最大消息数目,为0时禁止接收消息
+                 (OS_TICK	  )0,					//当使能时间片轮转时的时间片长度，为0时为默认长度，
+                 (void   	* )0,					//用户补充的存储区
+                 (OS_OPT      )OS_OPT_TASK_STK_CHK|OS_OPT_TASK_STK_CLR, //任务选项
+                 (OS_ERR 	* )&err);				//存放该函数错误时的返回值
+	OS_CRITICAL_EXIT();	//退出临界区	 
+	OSStart(&err);  //开启UCOSIII
+	while(1);
+}
+
+//开始任务函数
+void start_task(void *p_arg)
+{
+	OS_ERR err;
+	CPU_SR_ALLOC();
+	p_arg = p_arg;
+
+	CPU_Init();
+#if OS_CFG_STAT_TASK_EN > 0u
+   OSStatTaskCPUUsageInit(&err);  	//统计任务                
+#endif
+	
+#ifdef CPU_CFG_INT_DIS_MEAS_EN		//如果使能了测量中断关闭时间
+    CPU_IntDisMeasMaxCurReset();	
+#endif
+
+#if	OS_CFG_SCHED_ROUND_ROBIN_EN  //当使用时间片轮转的时候
+	 //使能时间片轮转调度功能,时间片长度为1个系统时钟节拍，既1*5=5ms
+	OSSchedRoundRobinCfg(DEF_ENABLED,1,&err);  
+#endif		
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_CRC,ENABLE);//开启CRC时钟
+	WM_SetCreateFlags(WM_CF_MEMDEV); //启动所有窗口的存储设备
+	GUI_Init();  			//STemWin初始化
+	
+	OS_CRITICAL_ENTER();	//进入临界区
+	//STemWin Demo任务	
+	OSTaskCreate((OS_TCB*     )&EmwindemoTaskTCB,		
+				 (CPU_CHAR*   )"Emwindemo task", 		
+                 (OS_TASK_PTR )emwindemo_task, 			
+                 (void*       )0,					
+                 (OS_PRIO	  )EMWINDEMO_TASK_PRIO,     
+                 (CPU_STK*    )&EMWINDEMO_TASK_STK[0],	
+                 (CPU_STK_SIZE)EMWINDEMO_STK_SIZE/10,	
+                 (CPU_STK_SIZE)EMWINDEMO_STK_SIZE,		
+                 (OS_MSG_QTY  )0,					
+                 (OS_TICK	  )0,  					
+                 (void*       )0,					
+                 (OS_OPT      )OS_OPT_TASK_STK_CHK|OS_OPT_TASK_STK_CLR,
+                 (OS_ERR*     )&err);
+			/*					 
+								 	OSTaskCreate((OS_TCB*     )&WatchTaskTCB,		
+				 (CPU_CHAR*   )"Watch task", 		
+                 (OS_TASK_PTR )watch_task, 			
+                 (void*       )0,					
+                 (OS_PRIO	  )WATCH_TASK_PRIO,     
+                 (CPU_STK*    )&WATCH_TASK_STK[0],	
+                 (CPU_STK_SIZE)WATCH_STK_SIZE/10,	
+                 (CPU_STK_SIZE)WATCH_STK_SIZE,		
+                 (OS_MSG_QTY  )0,					
+                 (OS_TICK	  )0,  					
+                 (void*       )0,					
+                 (OS_OPT      )OS_OPT_TASK_STK_CHK|OS_OPT_TASK_STK_CLR,
+                 (OS_ERR*     )&err);
+								 */
+								 
+								 		//按键配置入网						 
+	OSTaskCreate((OS_TCB*     )&KeyTaskTCB,		
+				 (CPU_CHAR*   )"Key task", 		
+                 (OS_TASK_PTR )key_task, 			
+                 (void*       )0,					
+                 (OS_PRIO	  )KEY_TASK_PRIO,     
+                 (CPU_STK*    )&KEY_TASK_STK[0],	
+                 (CPU_STK_SIZE)KEY_STK_SIZE/10,	
+                 (CPU_STK_SIZE)KEY_STK_SIZE,		
+                 (OS_MSG_QTY  )0,					
+                 (OS_TICK	  )0,  					
+                 (void*       )0,					
+                 (OS_OPT      )OS_OPT_TASK_STK_CHK|OS_OPT_TASK_STK_CLR,
+                 (OS_ERR*     )&err);
+								 
+								 
+								 
+	//触摸屏任务
+	OSTaskCreate((OS_TCB*     )&TouchTaskTCB,		
+				 (CPU_CHAR*   )"Touch task", 		
+                 (OS_TASK_PTR )touch_task, 			
+                 (void*       )0,					
+                 (OS_PRIO	  )TOUCH_TASK_PRIO,     
+                 (CPU_STK*    )&TOUCH_TASK_STK[0],	
+                 (CPU_STK_SIZE)TOUCH_STK_SIZE/10,	
+                 (CPU_STK_SIZE)TOUCH_STK_SIZE,		
+                 (OS_MSG_QTY  )0,					
+                 (OS_TICK	  )0,  					
+                 (void*       )0,					
+                 (OS_OPT      )OS_OPT_TASK_STK_CHK|OS_OPT_TASK_STK_CLR,
+                 (OS_ERR*     )&err);			 
+	//LED0任务
+	OSTaskCreate((OS_TCB*     )&Led0TaskTCB,		
+				 (CPU_CHAR*   )"Led0 task", 		
+                 (OS_TASK_PTR )led0_task, 			
+                 (void*       )0,					
+                 (OS_PRIO	  )LED0_TASK_PRIO,     
+                 (CPU_STK*    )&LED0_TASK_STK[0],	
+                 (CPU_STK_SIZE)LED0_STK_SIZE/10,	
+                 (CPU_STK_SIZE)LED0_STK_SIZE,		
+                 (OS_MSG_QTY  )0,					
+                 (OS_TICK	  )0,  					
+                 (void*       )0,					
+                 (OS_OPT      )OS_OPT_TASK_STK_CHK|OS_OPT_TASK_STK_CLR,
+                 (OS_ERR*     )&err);				 
+	OS_TaskSuspend((OS_TCB*)&StartTaskTCB,&err);		//挂起开始任务			 
+	OS_CRITICAL_EXIT();	//退出临界区
+}
+
+//EMWINDEMO任务
+void emwindemo_task(void *p_arg)
+{
+	GUI_CURSOR_Show(); 
+	
+	//更换皮肤
+	BUTTON_SetDefaultSkin(BUTTON_SKIN_FLEX); 
+	CHECKBOX_SetDefaultSkin(CHECKBOX_SKIN_FLEX);
+	DROPDOWN_SetDefaultSkin(DROPDOWN_SKIN_FLEX);
+	FRAMEWIN_SetDefaultSkin(FRAMEWIN_SKIN_FLEX);
+	HEADER_SetDefaultSkin(HEADER_SKIN_FLEX);
+	MENU_SetDefaultSkin(MENU_SKIN_FLEX);
+	MULTIPAGE_SetDefaultSkin(MULTIPAGE_SKIN_FLEX);
+	PROGBAR_SetDefaultSkin(PROGBAR_SKIN_FLEX);
+	RADIO_SetDefaultSkin(RADIO_SKIN_FLEX);
+	SCROLLBAR_SetDefaultSkin(SCROLLBAR_SKIN_FLEX);
+	SLIDER_SetDefaultSkin(SLIDER_SKIN_FLEX);
+	SPINBOX_SetDefaultSkin(SPINBOX_SKIN_FLEX); 
+	while(1)
+	{
+		ButtonUse_Demo();  
+	}
+}
+
+//TOUCH任务
+void touch_task(void *p_arg)
+{
+	OS_ERR err;
+	while(1)
+	{
+		GUI_TOUCH_Exec();	
+		OSTimeDlyHMSM(0,0,0,5,OS_OPT_TIME_PERIODIC,&err);//延时5ms
+	}
+}
+
+//LED0任务
+void led0_task(void *p_arg)
+{
+	OS_ERR err;
+	while(1)
+	{
+		LED0 = !LED0;
+		OSTimeDlyHMSM(0,0,0,500,OS_OPT_TIME_PERIODIC,&err);//延时500ms
+	}
+}
+
+
+/*void watch_task(void *p_arg)
+{
+		char text_buffer[30]={0};
+		OS_ERR err;
+		while(1)
+		{
+			OSTimeDlyHMSM(0,0,1,0,OS_OPT_TIME_PERIODIC,&err);	
+			sprintf(text_buffer,"MUSIC player                  CPU:%02d%%",(uint8_t)(OSStatTaskCPUUsage/100));
+			FRAMEWIN_SetText(hWin1, text_buffer);
+		}
+}*/
+
